d9dfdea68f0af1cabbda8f18019ba421355d387e
Udit Kumar Chatterjee <quantumudit@gmail.com>
2024-03-29 12:39:46 +0530

    solves the challenge in text splitting with python

---
+++ solutions/python/2024/Q1/ch2024_01_04.py
@@ -0,0 +1,76 @@
+"""
+This module provides a function to determine if a given year is a leap year in either
+the Gregorian or Revised Julian calendar, but not both.
+
+This script solves the challenge by filtering the leap years based on the
+given condition from 1901 to 9999.
+"""
+
+import string
+
+# Import necessary libraries
+import pandas as pd
+
+
+def char_type(char: str) -> str:
+    """
+    This function takes a single character as input and returns
+    the type of the character.
+
+    Args:
+        char (str): A single character string.
+
+    Returns:
+        str: The type of the character. It returns "lower" if the character is a
+        lowercase letter, "upper" if the character is an uppercase letter, "digit"
+        if the character is a digit, and "special" for any other characters.
+    """
+    if char in string.ascii_lowercase:
+        group = "lower"
+    elif char in string.ascii_uppercase:
+        group = "upper"
+    elif char in string.digits:
+        group = "digit"
+    else:
+        group = "special"
+    return group
+
+
+def text_transform(text: str) -> str:
+    """
+    This function transforms a given text by inserting a comma between characters
+    of different types.
+
+    Args:
+        text (str): The input string that needs to be transformed.
+
+    Returns:
+        str: The transformed string with commas inserted between characters
+        of different types.
+    """
+    text_len = len(text)
+    letters = ""
+    for idx in range(text_len):
+        current_char_type = char_type(text[idx])
+        prev_char_type = char_type(text[idx - 1])
+        if idx > 0:
+            if current_char_type != prev_char_type:
+                letters += f", {text[idx]}"
+            else:
+                letters += text[idx]
+        else:
+            letters += text[idx]
+    return letters
+
+
+if __name__ == "__main__":
+    # Loading the data
+    DATA_PATH = "./data/2024/Q1/ch2024_01_04.csv"
+    df = pd.read_csv(DATA_PATH)
+    data = df["Data"]
+
+    # Transform the texts
+    transformed_text = [text_transform(text) for text in data]
+
+    # Print the transformed texts
+    print("\n".join(transformed_text))
